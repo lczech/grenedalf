ffb5aa77184510495ecaa52bd27b45e699a6bbfa
Lucas Czech <luc.czech@gmail.com>
2023-06-20 18:26:05 -0700

    Release v0.2.0

--- src/tools/version.hpp
+++ src/tools/version.hpp
@@ -32,7 +32,7 @@
 
 inline std::string grenedalf_version()
 {
-    return "v0.2.0-beta"; // #GRENEDALF_VERSION#
+    return "v0.2.0"; // #GRENEDALF_VERSION#
 }
 
 inline std::string grenedalf_header()
