5eeab7b997d8ab8b38f96fca97e00a34a5354526
Lucas Czech <luc.czech@gmail.com>
2024-06-03 20:41:18 +0200

    Add bam.gz wildcard search

--- src/options/variant_file_sam.cpp
+++ src/options/variant_file_sam.cpp
@@ -54,7 +54,10 @@ CLI::Option* VariantFileSamOptions::add_file_input_opt_to_app_(
 
     // Add the option
     file_input_.add_multi_file_input_opt_to_app(
-        sub, "sam", "sam/bam/cram", "(sam(\\.gz)?|bam|cram)", ".sam[.gz]|.bam|.cram", required, group
+        sub, "sam", "sam/bam/cram",
+        "(sam(\\.gz)?|bam(\\.gz)?|cram)",
+        ".sam[.gz]|.bam[.gz]|.cram",
+        required, group
     );
 
     // Min mapping quality
